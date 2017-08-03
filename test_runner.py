62bbc8fd59ab4bf708740c964126839cb72d06cf
Jeffrey Effendy <wawa_union.zone@live.com>
2017-08-04 03:29:49 +0800

    add test runner

---
+++ test_runner.py
@@ -0,0 +1,12 @@
+""" main module to run the tests """
+
+
+# core modules
+import unittest
+
+# test moules
+from tests.test_todo import TodoTestCase
+
+
+if __name__ == "__main__":
+    unittest.main()
